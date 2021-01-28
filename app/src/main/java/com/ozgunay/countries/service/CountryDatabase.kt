44dbdcba7c22cfe2dc70511f4e42f025e1ff69e6
ozgunay41 <ozgun-41@hotmail.com>
2021-01-28 21:58:25 +0300

    remove comment lines

--- app/src/main/java/com/ozgunay/countries/service/CountryDatabase.kt
+++ app/src/main/java/com/ozgunay/countries/service/CountryDatabase.kt
@@ -11,18 +11,18 @@ abstract class CountryDatabase : RoomDatabase() {
 
     abstract fun countryDao(): CountryDao
 
-    //Singleton
 
-    //sadece 1 obje oluşturmak için kullanılır. Obje varsa onu kullanır. Her sınıftan veya fragmenttan ulaşılabilir.
+
+
     companion object {
 
-        //volatile ekleyerek farklı threadlerde çağırılabilir.
+
         @Volatile
         private var instance: CountryDatabase? = null
 
         private val lock = Any()
 
-        //instance objemizin olup olmadığını kontrol etmek için yazılan fonksiyon. Yoksa oluşturuyor.varsa olanı döndürüyor.
+
         operator fun invoke(context: Context) = instance
             ?: synchronized(lock) {  //birden fazla thread instance'a erişmeye çalışırsa sadece bir tanesine izin verir.
                 instance ?: makeDatabase(context).also {
